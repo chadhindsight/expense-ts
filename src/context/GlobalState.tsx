9739547e3fce5f92af2caba6c5beab03b8ab591c
chad hinds <chadrickj8@gmail.com>
2024-02-23 15:29:54 -0500

    Solidify state setup

---
+++ src/context/GlobalState.tsx
@@ -0,0 +1,36 @@
+import { createContext, useContext, ReactNode, useReducer } from 'react';
+import { appReducer } from "./AppReducer";
+import { AppContextProps } from '../types/transactionTypes';
+
+
+const defaultContextValue: AppContextProps = {
+    listOfTransactions: [],
+    dispatch: () => { },
+};
+
+export const AppContext = createContext<AppContextProps>(defaultContextValue);
+
+export const useAppContext = () => {
+    const establishedContext = useContext(AppContext);
+    if (!establishedContext) {
+        throw new Error('useAppContext must be used within an AppProvider');
+    }
+    return establishedContext
+}
+
+// Provider setup
+interface AppProviderProps {
+    children: ReactNode;
+}
+
+
+export const AppProvider = ({ children }: AppProviderProps) => {
+    const [listOfTransactions, dispatch] = useReducer(appReducer, []);
+
+    return (
+        <AppContext.Provider value={{ listOfTransactions, dispatch }
+        }>
+            {children}
+        </AppContext.Provider>
+    );
+};
