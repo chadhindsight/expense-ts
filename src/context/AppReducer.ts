3e5647b0c297c56424e0c34708b8bedce833989b
chad hinds <chadrickj8@gmail.com>
2024-02-23 15:29:45 -0500

    Create Reducer

--- src/context/AppReducer.ts
+++ src/context/AppReducer.ts
@@ -1,2 +1,15 @@
-// Initial setup of Reducer
-import { createContext, useContext, ReactNode, useReducer, Dispatch } from 'react';
+import { TransactionAction, TransactionList } from '../types/transactionTypes';
+
+
+export const appReducer = (state: TransactionList, action: TransactionAction) => {
+    switch (action.type) {
+        case 'SET_TRANSACTION':
+            return action.payload;
+
+        case 'DELETE_TRANSACTION':
+            return state.filter((item) => item.id !== action.payload);
+        case 'ADD_TRANSACTION':
+            return [...state, action.payload]
+    }
+};
+
