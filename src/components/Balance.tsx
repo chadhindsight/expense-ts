51d240b85e4c74d73cc9d5df701a9ae986ee0313
chad hinds <chadrickj8@gmail.com>
2024-02-26 10:42:06 -0500

    Create money formatter method and use it in Balance component

--- src/components/Balance.tsx
+++ src/components/Balance.tsx
@@ -1,8 +1,16 @@
+import { useContext } from "react";
+import { AppContext } from "../context/GlobalState";
+import { moneyFormatter } from "../utils/moneyFormatter";
 
 const Balance = () => {
+    const { listOfTransactions } = useContext(AppContext)
+    const amounts = listOfTransactions.map(transaction => transaction.amount)
+    const total = amounts.reduce((acc, item) => (acc += item), 0);
+
     return (
         <>
-
+            <h4>Your Current Balance</h4>
+            <h1>{moneyFormatter(total)}</h1>
         </>
     );
 };
