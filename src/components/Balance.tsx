e376b9ffa108bb5147471ce6e5c0a086300764ab
chad hinds <chadrickj8@gmail.com>
2024-02-26 10:56:46 -0500

    Leave placeholder comment

--- src/components/Balance.tsx
+++ src/components/Balance.tsx
@@ -3,10 +3,13 @@ import { AppContext } from "../context/GlobalState";
 import { moneyFormatter } from "../utils/moneyFormatter";
 
 const Balance = () => {
+    // Why is this empty after trying to add new transaction(global state issue maybe)?
     const { listOfTransactions } = useContext(AppContext)
+
     const amounts = listOfTransactions.map(transaction => transaction.amount)
     const total = amounts.reduce((acc, item) => (acc += item), 0);
     console.log(listOfTransactions)
+
     return (
         <>
             <h4>Your Current Balance</h4>
