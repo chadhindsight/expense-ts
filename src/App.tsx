4da9944f2ea337c1bcf8447403393d89fc44133c
chad hinds <chadrickj8@gmail.com>
2024-02-26 10:42:15 -0500

    Import Balance component

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 // import { useState } from 'react'
 import './App.css'
 import { AddTransaction } from './components/AddTransaction'
+import Balance from './components/Balance'
 import { Header } from './components/presentational/Header'
 function App() {
   // const [count, setCount] = useState(0)
@@ -8,6 +9,7 @@ function App() {
   return (
     <>
       <Header />
+      <Balance />
       <AddTransaction />
     </>
   )
