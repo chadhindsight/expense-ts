59a041041408b7adcecec29eacd3f5e439685ad8
chad hinds <chadrickj8@gmail.com>
2024-03-08 14:29:02 -0500

    create deleteTransaction function

--- src/components/Transaction.tsx
+++ src/components/Transaction.tsx
@@ -1,6 +1,18 @@
-import { AppContext } from "../context/GlobalState";
+import { useContext } from "react";
+import { AppContext, useAppContext } from "../context/GlobalState";
 
-const Transaction = () => {
+const Transaction = ({ transaction }) => {
+    const { listOfTransactions } = useContext(AppContext);
+    const sign = transaction.amount < 0 ? '-' : '+';
+    const { dispatch } = useAppContext();
+
+    function deleteTransaction(id) {
+        dispatch({
+            type: 'DELETE_TRANSACTION',
+            payload: id
+        });
+    }
+
     return (
         <>
 
