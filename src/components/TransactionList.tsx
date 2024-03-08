c5041d7087218de0cb9cbcbafe1520b10abbb22e
chad hinds <chadrickj8@gmail.com>
2024-03-08 15:17:38 -0500

    Add transaction component

--- src/components/TransactionList.tsx
+++ src/components/TransactionList.tsx
@@ -1,5 +1,5 @@
 import { useContext } from 'react';
-import { TransactionComp } from './Transaction';
+import TransactionComp from './Transaction';
 
 import { AppContext } from '../context/GlobalState';
 
